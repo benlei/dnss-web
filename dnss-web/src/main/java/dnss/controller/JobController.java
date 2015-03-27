571d09fc8d111b54718b8033d3ebda306b9a56d6
Benjamin Lei <benlei.cse@gmail.com>
2015-03-27 01:45:26 -0700

    Unneeeded import.

--- dnss-web/src/main/java/dnss/controller/JobController.java
+++ dnss-web/src/main/java/dnss/controller/JobController.java
@@ -1,7 +1,6 @@
 package dnss.controller;
 
 import dnss.model.Job;
-import dnss.enums.Advancement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
