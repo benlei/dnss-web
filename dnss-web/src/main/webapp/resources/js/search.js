b70e311aa1103f81b6829b3396839473e51d42d8
Benjamin Lei <benlei.cse@gmail.com>
2015-05-14 00:43:29 -0700

    Fixing search issue where it can have false negatives.

--- dnss-web/src/main/webapp/resources/js/search.js
+++ dnss-web/src/main/webapp/resources/js/search.js
@@ -7,18 +7,19 @@ var search = new (function Search() {
   }
 
   function nameAndDescription(pattern) {
-    var re = new RegExp(pattern, "igm"), skills = dnss.getSkills();
+    var re = new RegExp(pattern, "igm");
+    var skills = dnss.getSkills();
     for (id in skills) {
       var skill = skills[id];
-      var haystacks = [skill.getName(), skill.getRequiredWeapons(), skill.getType(),
-        skill.getDescription(), skill.getNextDescription()];
+      var haystacks = [skill.getName(),
+                       skill.getRequiredWeapons(),
+                       skill.getType(),
+                       skill.getDescription(),
+                       skill.getNextDescription()].filter(function(v) { return v != -1 });
 
       var matched = false;
-      for (var i = 0 ; i < haystacks.length; i++) {
-        if (haystacks[i] != -1 && re.test(haystacks[i])) {
-          matched = true;
-          break;
-        }
+      for (var i = 0 ; i < haystacks.length && ! matched; i++) {
+        matched = re.test(haystacks[i]) || re.test(haystacks[i]); // need to test twice because regexp can have false negatives
       }
 
       skill.setOpacity(matched ? 1 : .1);
@@ -34,5 +35,5 @@ var search = new (function Search() {
     }
   };
 
-  $("#search").on("input", this.hook);
+  $("#search").val("").on("input", this.hook);
 })();
